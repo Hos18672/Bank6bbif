30a5dcb32bd6c1b6fa1d46d2f9b109d6a1fcfec1
Hos18672 <Hos18672@spengergasse.at>
2020-04-12 22:32:00 +0200

    add JDBC

--- src/domain/BankAccountException.java
+++ src/domain/BankAccountException.java
@@ -1,5 +1,10 @@
 package domain;
 
-public class BankAccountException {
+public class BankAccountException extends Exception {
 
+	private static final Long serialVersionUID = 1l; 
+	public BankAccountException(String massege)
+	{
+		super(massege);
+	}
 }
